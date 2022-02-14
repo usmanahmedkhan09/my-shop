50b162bc5be617d631d51d9ee8c7b014c2adb697
usman ahmed <usmanahmed.andpercent@gmail.com>
2022-02-14 12:37:18 +0500

    add orders ejs

--- controllers/shop.js
+++ controllers/shop.js
@@ -138,16 +138,14 @@ exports.postOrder = (req, res, next) =>
 
 exports.getOrders = (req, res, next) =>
 {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user.getOrders({ include: ['products'] }).then(orders =>
+  {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  }).catch(err => console.log(err));
+
 };
 
-exports.getCheckout = (req, res, next) =>
-{
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
